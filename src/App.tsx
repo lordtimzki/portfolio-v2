f04c686a308a8e204832235de81af970904a103b
lordtimzki <tdacalos@uci.edu>
2025-02-19 03:07:46 -0800

    gradient

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Corner from "./components/Corner";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-purp1 to-purp2 min-h-screen">
+      <Corner></Corner>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Corner.tsx
@@ -0,0 +1,9 @@
+const Corner = () => {
+  return (
+    <div className="text-white">
+      <h1 className="font-roboto font-thin">Timothy Dacalos</h1>
+    </div>
+  );
+};
+
+export default Corner;
